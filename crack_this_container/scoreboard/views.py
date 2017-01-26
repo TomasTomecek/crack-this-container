dfd60867328b0abee9d1c29314208c7e4a828e58
Tomas Tomecek <ttomecek@redhat.com>
2017-01-26 16:14:01 +0100

    don't accept solution if the game has not started yet
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- crack_this_container/scoreboard/views.py
+++ crack_this_container/scoreboard/views.py
@@ -40,6 +40,8 @@ def api_start_latest_game(request):
 def api_submit_solution(request):
     game = Game.objects.latest_game()
     solution = Solution.create(game)
+    if not game.has_started:
+        return HttpResponse("Game hasn't started yet. You can't be that quick.")
 
     redis_publisher = RedisPublisher(facility='solution-submitted', broadcast=True)
     message = RedisMessage(solution.listing_text)
